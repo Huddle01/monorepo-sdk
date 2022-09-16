65f4304b359cac29dfa1c35d9a891fc18b36cae4
harry-sketch <harshbisht90@gmail.com>
2022-09-16 16:26:00 +0530

    add: screens to tailwind config

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -10,58 +10,66 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        inter: ['Inter', 'sans-serif'],
+        inter: ["Inter", "sans-serif"],
       },
       colors: {
-        brand: '#246BFD',
+        brand: "#246BFD",
         custom: {
-          blue: '#246BFD',
-          black: '#121214',
-          navy: '#181A20',
-          1: '#1C1E24',
-          2: '#334155',
-          4: '#1C1E24',
-          5: '#23262F',
+          blue: "#246BFD",
+          black: "#121214",
+          navy: "#181A20",
+          1: "#1C1E24",
+          2: "#334155",
+          4: "#1C1E24",
+          5: "#23262F",
         },
         webRgbColors: {
-          1: 'rgba(24, 24, 27, 0.8)',
-          2: 'rgba(75, 85, 99, 0.1)',
-          3: 'rgba(71, 85, 105, 0.2)',
-          4: 'rgba(51, 65, 85, 0.2)',
-          5: 'rgba(24, 24, 28, 0.6)',
-          6: 'rgba(51, 65, 85, 0.3)',
-          7: 'rgba(71, 85, 105, 0.1)',
-          8: 'rgba(24, 26, 32, 0.9)',
+          1: "rgba(24, 24, 27, 0.8)",
+          2: "rgba(75, 85, 99, 0.1)",
+          3: "rgba(71, 85, 105, 0.2)",
+          4: "rgba(51, 65, 85, 0.2)",
+          5: "rgba(24, 24, 28, 0.6)",
+          6: "rgba(51, 65, 85, 0.3)",
+          7: "rgba(71, 85, 105, 0.1)",
+          8: "rgba(24, 26, 32, 0.9)",
         },
       },
       animation: {
-        'spin-fast': 'spin 0.7s linear infinite',
-        'toast-in': 'toast-in-right .7s',
-        roundtime: 'roundtime calc(4 * 1s) linear forwards',
+        "spin-fast": "spin 0.7s linear infinite",
+        "toast-in": "toast-in-right .7s",
+        roundtime: "roundtime calc(4 * 1s) linear forwards",
       },
       keyframes: {
-        'toast-in-right': {
+        "toast-in-right": {
           from: {
-            transform: 'translateX(100%)',
+            transform: "translateX(100%)",
           },
           to: {
-            transform: 'translateX(0)',
+            transform: "translateX(0)",
           },
         },
         roundtime: {
           to: {
-            transform: 'scaleX(0)',
+            transform: "scaleX(0)",
           },
         },
       },
       transitionTimingFunction: {
-        loading: 'cubic-bezier(0.65, 0, 0.35, 1)',
+        loading: "cubic-bezier(0.65, 0, 0.35, 1)",
+      },
+      screens: {
+        mob: "390px",
+        tablet: "768px",
+        laptop: "1024px",
+        hdReady: "1280px",
+        hdPlus: "1600px",
+        fullHd: "1920px",
       },
     },
   },
   variants: {
     extend: {
-      backgroundColor: ['checked'],
+      backgroundColor: ["checked"],
     },
   },
   darkMode: "class",
