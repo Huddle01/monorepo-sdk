3dd8804b12af66b54d0af5b1098519a569aa1ced
OmGuptaIND <omgupta0720@gmail.com>
2022-11-10 02:21:05 +0530

    add: sdk apiKeys

---
+++ apps/docs/pages/api/auth/create-api.ts
@@ -0,0 +1,62 @@
+import axios from "axios";
+import crypto from "crypto";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { hasuraUrl, __prod__ } from "../../../helpers";
+import { CREATE_API_KEY_FOR_ADDRESS } from "../../../helpers/graphql";
+import { createSdkApiKey } from "../../../helpers/validators";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { body } = req;
+    if (!body) {
+      throw new Error("No Body Provided");
+    }
+
+    const { address } = createSdkApiKey.parse(body);
+
+    const apiKey = crypto.randomBytes(32).toString("hex");
+
+    const resp = await axios.post(
+      hasuraUrl,
+      {
+        query: CREATE_API_KEY_FOR_ADDRESS,
+        variables: {
+          object: {
+            address: address.toLowerCase(),
+            apiKey,
+          },
+        },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "x-hasura-admin-secret": __prod__
+            ? process.env.HASURA_SECRET_PROD
+            : process.env.HASURA_SECRET_DEV,
+        },
+      }
+    );
+
+    const { data, status } = resp;
+
+    if (status > 299 && data.errors) {
+      console.error(data.errors);
+      throw new Error("Error Creating API Key");
+    }
+
+    res.status(201).json({
+      message: "API Key Created",
+      apiKey,
+    });
+
+    return;
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ statusCode: 500, message: "Internal Server Error" });
+
+    return;
+  }
+}
