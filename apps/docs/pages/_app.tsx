2869f799d10870865864515902d8079357cb080b
Akshit1311 <a4748g@gmail.com>
2022-11-22 22:13:20 +0530

    add: FAQs

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -2,8 +2,8 @@ import "../styles.css";
 import "../custom.css";
 
 import { SSRProvider } from "@react-aria/ssr";
-import type { AppProps } from "next/app";
-import type { ReactNode } from "react";
+import { AppProps } from "next/app";
+import { ReactNode } from "react";
 
 type NextraAppProps = AppProps & {
   Component: AppProps["Component"] & {
@@ -13,14 +13,46 @@ type NextraAppProps = AppProps & {
 
 // Shim requestIdleCallback in Safari
 if (typeof window !== "undefined" && !("requestIdleCallback" in window)) {
-  window.requestIdleCallback = (fn) => setTimeout(fn, 1);
-  window.cancelIdleCallback = (e) => clearTimeout(e);
+  window.requestIdleCallback = fn => setTimeout(fn, 1);
+  window.cancelIdleCallback = e => clearTimeout(e);
 }
+
+// web3
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [chain.mainnet],
+  [
+    alchemyProvider({ apiKey: "g14UUdgqZdm6jlMf8YU5wY44Uj0ULttz" }),
+    publicProvider()
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains
+});
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+});
 
 export default function Nextra({ Component, pageProps }: NextraAppProps) {
   return (
     <SSRProvider>
-      <Component {...pageProps} />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider coolMode theme={darkTheme()} chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </SSRProvider>
   );
 }
--- apps/docs/pages/docs/faq.md
+++ apps/docs/pages/docs/faq.md
@@ -1,6 +1,19 @@
 ## FAQs
 
-### **FAQs**
+### **Web SDK**
 
-- I have some doubts while writing code, where do I reach out?
-  - Post your doubts on #specific-thread on our Discord (add link here) channel
+- How do I get started ?
+  - You will find your API Key under the Web SDK unde the [Get Your API Key](/docs/web-sdk/api-key) section
+- Is there a way to restart the socket connection
+  - You can use the `setSocket()` method to reset the socket connection
+- Can I get the state of my video application using the SDK ?
+  - Yes you can use the `useHuddleStore()` to subscribe to app state changes
+
+### **IFrame SDK**
+
+- How do I get started ?
+  - Head to the IFrame section and start with Quick Start guide
+- Can I connect my ethereum wallet from outside my Iframe ?
+  - You can implement your own wallet authentication in your app and then use the `connectWallet()` method in the IframeSDK to connect using that auth logic.
+- Can I modify the UI for my Iframe App ?
+  - Yes, you can contact the Huddle01 team for customization, self-customization is still in the works!
